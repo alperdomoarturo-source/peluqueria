6acbe5a51be6a3c09031c5bd73bf42bac95d8fff
alvaro <alperdomoarturo@gmail.com>
2026-07-31 11:52:19 -0500

    Revert to BrowserRouter and remove debug code

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/Home'
 import Booking from './pages/Booking'
 import AdminLogin from './pages/admin/AdminLogin'
@@ -8,31 +8,9 @@ import AdminServices from './pages/admin/AdminServices'
 import AdminSchedules from './pages/admin/AdminSchedules'
 import { isAuthenticated } from './utils/auth'
 
-function DebugLocation() {
-  const location = useLocation()
-  console.log('Current location:', location.pathname)
-  return (
-    <div style={{
-      position: 'fixed',
-      top: 0,
-      left: 0,
-      right: 0,
-      background: 'yellow',
-      color: 'black',
-      padding: '10px',
-      zIndex: 9999,
-      textAlign: 'center',
-      fontSize: '14px'
-    }}>
-      Ruta actual: {location.pathname}
-    </div>
-  )
-}
-
 function App() {
   return (
     <Router>
-      <DebugLocation />
       <Routes>
         {/* Client routes */}
         <Route path="/" element={<Home />} />
