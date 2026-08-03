26694b0a8eed5bd18f8a1505979ba85a153a807e
alvaro <alperdomoarturo@gmail.com>
2026-08-02 22:13:25 -0500

    Reduce background image size on mobile to 5%

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -49,7 +49,7 @@ export default function Home() {
         <style>{`
           @media (max-width: 767px) {
             #services {
-              background-size: 10% auto;
+              background-size: 5% auto;
               background-repeat: repeat;
             }
           }
